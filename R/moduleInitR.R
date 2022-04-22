8dc1b35f8f7c62191a5edefd06ce4cde9511b0bd
Jordan Hayes <hayesjordan@gmail.com>
2022-04-21 17:46:52 -0700

    Added auto init

--- R/moduleInitR.R
+++ R/moduleInitR.R
@@ -1,7 +1,5 @@
 # define modules runtine quarantine configuration
 Sys.setenv('MODULES_RUN_QUARANTINE'='LD_LIBRARY_PATH LD_PRELOAD')
-Sys.setenv('MODULEPATH'='/opt/linux/rocky/8.x/x86_64/modules')
-Sys.setenv('HPCC_MODULES'='/opt/linux/rocky/8.x/x86_64/pkgs')
 
 # setup quarantine if defined
 mlre <- ''
@@ -25,3 +23,33 @@ if (!is.na(Sys.getenv('MODULES_RUN_QUARANTINE', unset=NA))) {
 cmdpipe <- pipe(paste0(mlre, '/usr/bin/tclsh /usr/share/Modules/libexec/modulecmd.tcl r autoinit'))
 eval(parse(cmdpipe))
 close(cmdpipe)
+
+.onLoad <- function(libname, pkgname) {
+  # Module function assumes MODULEPATH and MODULEDIR are set in login profile
+  # Get base environment from login profile
+  base_env <- strsplit(system('bash -l -c "env"',intern = TRUE),'\n')
+  base_env <- strsplit(as.character(base_env),'=')
+
+  # Iterate through base environment
+  for (x in seq(1,length(base_env))) {
+
+    # Set environment based on login profile
+    if (base_env[[x]][1]=="LOADEDMODULES" || base_env[[x]][1]=="MODULESHOME" || base_env[[x]][1]=="MODULEPATH" || base_env[[x]][1]=="MODULES_DIR" || base_env[[x]][1]=="HPCC_MODULES"){
+      if (base_env[[x]][1]=="LOADEDMODULES"){
+        default_modules <- strsplit(base_env[[x]][2],":")
+      }
+      else{
+        l <- list(base_env[[x]][2])
+        names(l) <- base_env[[x]][1]
+        do.call(Sys.setenv, l)
+      }
+    }
+  }
+
+  # Make sure to process default modules after the environment is set with the above loop
+  for (x in seq(1,length(default_modules[[1]]))){
+    module_name <- default_modules[[1]][x]
+    print(paste("Loading module",module_name))
+    try(eval(paste('module("load",',module_name,')')))
+  }
+}
